3a40dd07b2e44d796ab8814b3696b6480fa21f9d
egor-bogomolov <msicar.ot.on.yas@gmail.com>
2018-04-09 23:22:03 +0300

    File reader and ast parser

--- Java/src/main/java/Main.java
+++ Java/src/main/java/Main.java
@@ -1,5 +1,37 @@
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
+
+    private static List<String> readFile(String filename) {
+        try(BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+            List<String> lines = new ArrayList<>();
+            String line;
+            while((line = reader.readLine()) != null) {
+                lines.add(line);
+            }
+            return lines;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return Collections.emptyList();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        String filename = args[0];
+        int lineNumber = Integer.valueOf(args[1]) - 1;
+        List<String> lines = readFile(filename);
+        String replacementLine = lines.get(0);
+        lines.remove(0);
+        lines.remove(1);
+        lines.add(lineNumber, replacementLine);
+        CompilationUnit cu = JavaParser.parse(String.join(System.getProperty("line.separator"), lines));
+        System.out.println(cu.toString());
+
     }
 }
